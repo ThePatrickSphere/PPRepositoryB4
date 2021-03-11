7353cbeafa7ed724aa7580c147d60c03a99fb122
ThePatrickSphere <79872885+ThePatrickSphere@users.noreply.github.com>
2021-03-11 16:38:19 -0700

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -7,17 +7,21 @@ Program Goals:
     b. Return value at specific index
 
 """
-
+myList = []
 def mainProgram():
-    myList = []
-    print("Hello, there! Let's work the some lists!")
-    print("Choose one of the following options. Type a number below.")
-    choice = input("1. Add to list, 2. Return value at an index position.  ")
-    if choice == "1":
-        addToList()
-    elif choice == "2":
-        indexValues()
-    # To Add: 1. A way to loop action, 2. A loop to quit, 3. Repetition
+    while True:
+        print("Hello, there! Let's work the some lists!")
+        print("Choose one of the following options. Type a number below.")
+        choice = input(""" 1. Add to a list or
+2. Reurn a value at an index
+3. Exit program""")
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "3":
+            break
+        # To Add: 1. A way to loop action, 2. A loop to quit, 3. Repetition
 
 def addToList():
     print("Adding to a list...")
@@ -25,6 +29,9 @@ def addToList():
     myList.append(int(newItem))
 
 def indexValues():
+    print ("Getting a particular piece of data...")
+    indexPos = input("What index position are you curious about?     ")
+    print(myList [int(indexPos)])
 
 if __name__ == "__main__":
     mainProgram()
