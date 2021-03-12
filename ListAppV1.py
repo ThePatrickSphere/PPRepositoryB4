095210a744a4977c4210414e9cfa9748f09e53dd
ThePatrickSphere <79872885+ThePatrickSphere@users.noreply.github.com>
2021-03-12 13:12:42 -0700

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -7,21 +7,28 @@ Program Goals:
     b. Return value at specific index
 
 """
+import random
 myList = []
 def mainProgram():
     while True:
-        print("Hello, there! Let's work the some lists!")
-        print("Choose one of the following options. Type a number below.")
-        choice = input(""" 1. Add to a list or
-2. Reurn a value at an index
-3. Exit program""")
-        if choice == "1":
-            addToList()
-        elif choice == "2":
-            indexValues()
-        elif choice == "3":
-            break
-        # To Add: 1. A way to loop action, 2. A loop to quit, 3. Repetition
+        try:
+            print("Hello, there! Let's work with some lists!")
+            print("Choose one of the following options. Type a number below.")
+            choice = input(""" 1. Add to a list or
+2. Return a value at an index
+3. Random search
+4. Exit program""")
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                indexValues()
+            elif choice == "3":
+                randomSearch()
+            else:
+                break
+            
+        except:
+             print("You made an mistake!")
 
 def addToList():
     print("Adding to a list...")
@@ -32,6 +39,10 @@ def indexValues():
     print ("Getting a particular piece of data...")
     indexPos = input("What index position are you curious about?     ")
     print(myList [int(indexPos)])
+
+def randomSearch():
+    print("Randomly searching...")
+    print(myList[random.randint(0, len(myList)-1)])
 
 if __name__ == "__main__":
     mainProgram()
