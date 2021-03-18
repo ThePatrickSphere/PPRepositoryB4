062bdf09285e0506b9239c08d443ae1bbcce0f85
ThePatrickSphere <79872885+ThePatrickSphere@users.noreply.github.com>
2021-03-18 13:24:08 -0600

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -15,15 +15,21 @@ def mainProgram():
             print("Hello, there! Let's work with some lists!")
             print("Choose one of the following options. Type a number below.")
             choice = input(""" 1. Add to a list or
-2. Return a value at an index
-3. Random search
-4. Exit program""")
+2. Add a bunch of numbers
+3. Return a value at an index
+4. Random search
+5. Print contents of list
+6. Exit program""")
             if choice == "1":
                 addToList()
             elif choice == "2":
-                indexValues()
+                addABunch()
             elif choice == "3":
+                indexValues()
+            elif choice == "4":
                 randomSearch()
+            elif choice == "5":
+                print(myList)
             else:
                 break
             
@@ -35,6 +41,14 @@ def addToList():
     newItem = input("Type an integer here.")
     myList.append(int(newItem))
 
+def addABunch():
+    print("Adding a bunch of integers here...")
+    numberToAdd = input("How many integers would you like to add?   ")
+    numberRange = input("And how high would you like these numbers to go?   ")
+    for x in range(0, int(numberToAdd)):
+        myList.append(random.randint(0, int(numberRange)))
+    print("Your list is now complete!")
+
 def indexValues():
     print ("Getting a particular piece of data...")
     indexPos = input("What index position are you curious about?     ")
@@ -43,6 +57,13 @@ def indexValues():
 def randomSearch():
     print("Randomly searching...")
     print(myList[random.randint(0, len(myList)-1)])
+
+def linearSearch():
+    print("Checking out each item one at a time in your list...")
+    searchItem = input("What're you lookin' for?     ")
+    for x in range(len(myList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at index position {}".format(x))
 
 if __name__ == "__main__":
     mainProgram()
