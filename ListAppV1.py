ac52d921d50e575849d46960057bbf3bb2429e3f
ThePatrickSphere <79872885+ThePatrickSphere@users.noreply.github.com>
2021-04-18 11:46:43 -0600

    Add files via upload

--- ListAppV1.py
+++ ListAppV1.py
@@ -49,12 +49,22 @@ def mainProgram():
             
         except:
              print("You made a mistake! Try again.")
-
+"""
+addToList()
+The function adds individual numbers to the list by
+asking for user input and then appending the list with the integer.
+"""
 def addToList():
     print("Adding to a list...")
     newItem = input("Type an integer here.")
     myList.append(int(newItem))
-
+"""
+addABunch()
+This function adds multiple numbers at a time by asking for user input for
+2 things: how many numbers to add, and the maximum range of these numbers.
+Then the for loop appends the list with random numbers ranging from 0 to the
+"how high" input until the "how much" input is reached.
+"""
 def addABunch():
     print("Adding a bunch of integers here...")
     numberToAdd = input("How many integers would you like to add?   ")
@@ -62,12 +72,21 @@ def addABunch():
     for x in range(0, int(numberToAdd)):
         myList.append(random.randint(0, int(numberRange)))
     print("Your list is now complete! Type 10 to see list.")
-
+"""
+indexValues()
+This one searches for what number is at a given index position.
+When the user inputs an index value, the print statement prints the number in the specific index value.
+"""
 def indexValues():
     print ("Getting a particular piece of data...")
     indexPos = input("What index position are you curious about?     ")
     print(myList[int(indexPos)])
-
+"""
+sortList(myList)
+This function adds to the list unique_list through a for loop that
+appends unique numbers to it if not already there.
+The unique_list is then sorted numerically. The input asks the user if they want to see the unique_list.
+"""
 def sortList(myList):
     print("Sorting your list...")
     for x in myList:
@@ -77,20 +96,34 @@ def sortList(myList):
     showMe = input("Do you want to see your new list?  Y/N")
     if showMe.lower() == "y":
         print(unique_list)
-
+"""
+randomSearch()
+This function prints a random number in the list through returning
+the value stored at a random index position in range.
+"""
 def randomSearch():
     print("Randomly searching...")
     print(myList[random.randint(0, len(myList)-1)])
     if len(unique_list) > 0:
         print(unique_list[random.randint(0, len(unique_list)-1)])
-
+"""
+linearSearch()
+The function here looks through the list one number
+at a time until it finds the input term the user is looking for.
+It then prints the index position that search term is located at.
+"""
 def linearSearch():
     print("Checking out each item one at a time in your list...")
     searchItem = input("What're you lookin' for?     ")
     for x in range(len(myList)):
         if myList[x] == int(searchItem):
             print("Your item is at index position {}".format(x))
-
+"""
+recursiveBinarySearch(unique_list, low, high, x)
+This searches through the unique list through repeating a process of
+dividing the list in two. It assigns certain numbers low, mid, and high
+until the search term is found in the mid position.
+"""
 def recursiveBinarySearch(unique_list, low, high, x):
     if high >= low:
         mid = (high + low) // 2
@@ -104,7 +137,13 @@ def recursiveBinarySearch(unique_list, low, high, x):
             return recursiveBinarySearch(unique_list, mid + 1, high, x)
     else:
         print("Your number isn't here.")
-
+"""
+iterativeBinarySearch(unique_list, x)
+This is similar to recursive binary search in that it
+searches for the midpoint in a list to return the search term.
+But the function here keeps the midpoint the same and adds or subtracts
+the low and high points until the search term is the mid point.
+"""
 def iterativeBinarySearch(unique_list, x):
     low = 0
     high = len(unique_list)-1
@@ -120,7 +159,13 @@ def iterativeBinarySearch(unique_list, x):
         else:
             return mid
     return -1
-
+"""
+removeNumber()
+This self-created function removes numbers through the list depending on the
+contents of each list. If the unique list is empty, the function removes a
+number from myList. If not, it will remove numbers from both lists
+and print which one was removed.
+"""
 def removeNumber():
     print("Deleting a number...")
     deleteItem = input("Which number would you like to remove?    ")
@@ -132,7 +177,12 @@ def removeNumber():
         myList.remove(int(deleteItem))
         print(unique_list)
     print("The number {} was removed from your list!".format(deleteItem))
-
+"""
+printLists()
+This prints lists depending on the length of the unique_list. If empty, myList
+will be printed automatically. If not, the function will ask the user
+to print either the sorted list (unique_list) or the unsorted list(myList).
+"""
 def printLists():
     if len(unique_list) == 0:
         print(myList)
@@ -143,6 +193,9 @@ def printLists():
         else:
             print(myList)
         
-
+"""
+This is the "dunder name, dunder main" statement that calls the main program
+into action if it were to run as a standalone app.
+"""
 if __name__ == "__main__":
     mainProgram()
